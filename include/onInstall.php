a320501c21fe9ce5240b99007a4add064aa028fe
mambax7 <mambax7@gmail.com>
2019-04-10 11:02:46 -0400

    utility
    Signed-off-by: mambax7 <mambax7@gmail.com>

--- include/onInstall.php
+++ include/onInstall.php
@@ -1,27 +1,18 @@
 <?php
+
+use XoopsModules\Tad_book3\Utility;
+
+include dirname(__DIR__) . '/preloads/autoloader.php';
+
 function xoops_module_install_tad_book3(&$module)
 {
-
-    tad_book3_mk_dir(XOOPS_ROOT_PATH . "/uploads/tad_book3");
-    tad_book3_mk_dir(XOOPS_ROOT_PATH . "/uploads/tad_book3/file");
-    tad_book3_mk_dir(XOOPS_ROOT_PATH . "/uploads/tad_book3/image");
-    tad_book3_mk_dir(XOOPS_ROOT_PATH . "/uploads/tad_book3/image/.thumbs");
+    
+    Utility::mk_dir(XOOPS_ROOT_PATH . "/uploads/tad_book3");
+    Utility::mk_dir(XOOPS_ROOT_PATH . "/uploads/tad_book3/file");
+    Utility::mk_dir(XOOPS_ROOT_PATH . "/uploads/tad_book3/image");
+    Utility::mk_dir(XOOPS_ROOT_PATH . "/uploads/tad_book3/image/.thumbs");
 
     return true;
 }
 
-//建立目錄
-function tad_book3_mk_dir($dir = "")
-{
-    //若無目錄名稱秀出警告訊息
-    if (empty($dir)) {
-        return;
-    }
 
-    //若目錄不存在的話建立目錄
-    if (!is_dir($dir)) {
-        umask(000);
-        //若建立失敗秀出警告訊息
-        mkdir($dir, 0777);
-    }
-}
