fe3c5bfb9e815405dd053d7d711beff7cd76c7c6
tad0616 <tad0616@gmail.com>
2019-05-06 22:03:51 +0800

    避免區塊引不到物件

--- blocks/tad_book3_index.php
+++ blocks/tad_book3_index.php
@@ -1,6 +1,9 @@
 <?php
 use XoopsModules\Tadtools\Dtree;
 use XoopsModules\Tadtools\Utility;
+if (!class_exists('XoopsModules\Tadtools\Utility')) {
+    require XOOPS_ROOT_PATH . '/modules/tadtools/preloads/autoloader.php';
+}
 
 //區塊主函式 (會自動偵測目前閱讀的書籍，並秀出該書目錄)
 function tad_book3_index()
